90c6554da287b3300b78cd0c354e0c0d1dc6a8a7
dbm3 <dbm3@TheBeast>
2017-02-11 12:25:39 -0800

    Began work on logic for document parser

---
+++ src/codeswap/Word.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codeswap;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author dbm3
+ */
+public class Word {
+    private String value;
+    private boolean flagged = false;
+    private enum flaggedFor {deletion, swap;}
+    private Color formattedColor;
+    public Word(String value){
+        this.value = value;
+    }
+    
+    //add get/sets
+}
--- src/codeswap/analyzeParser.java
+++ src/codeswap/analyzeParser.java
@@ -5,10 +5,55 @@
  */
 package codeswap;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author dbm3
  */
 public class analyzeParser {
+    private File file;
+    BufferedReader br;
+    public analyzeParser(File file){
+        this.file = file;
+    }
     
+    public void analyze(){
+        try {
+            Scanner docParser = new Scanner(file);
+            Scanner lineParser = new Scanner("");
+            lineParser.useDelimiter(" \n\r.()[]{}");
+            while(docParser.hasNext()){
+                lineParser = new Scanner(docParser.nextLine());
+                while(lineParser.hasNext()){
+                    if(shouldNextBeFlagged(lineParser.next())){
+                        
+                    }
+                }
+            }
+            
+            
+            
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(analyzeParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    private boolean shouldNextBeFlagged(String word){
+        switch(word){
+            case "int":
+            case "String":
+            case "char":
+                return true;
+            default:
+                return false;
+        }
+    }
 }
